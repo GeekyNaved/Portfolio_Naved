b80d660537d442210118da0f4089462c91640ebd
GeekyNaved <navedahmed040@gmail.com>
2023-12-21 15:18:06 +0530

    adding mail functionality in let's chat button

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,12 +2,12 @@ import SocialLinksGroup from "./SocialLinksGroup";
 
 const HeroSection = () => {
   return (
-    <div className="relative mx-auto text-center">
+    <div className="relative bg-green-500 mx-auto text-center py-10">
       <h1 className="text-4xl">Naved Khan</h1>
-      <p>Web & App Developer</p>
+      <p>Web and App Developer</p>
       <SocialLinksGroup />
       <a
-        href="#"
+        href="mailto:navedahmed040@gmail.com"
         className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md"
       >
         Let&apos;s Chat
