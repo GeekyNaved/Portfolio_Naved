df5fb7318d5c4361fd0401d4c268e3090ea9c55f
GeekyNaved <navedahmed040@gmail.com>
2023-12-22 14:47:52 +0530

    adding links in social icons

--- src/components/SocialLinksGroup.jsx
+++ src/components/SocialLinksGroup.jsx
@@ -1,18 +1,18 @@
-import {FaLinkedin, FaInstagramSquare, FaGithub} from "react-icons/fa";
-import {FaSquareXTwitter} from "react-icons/fa6";
+import { FaLinkedin, FaInstagramSquare, FaGithub } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
 const SocialLinksGroup = () => {
   return (
     <div className="flex items-center justify-center gap-5 text-3xl py-4 text-gray-500">
-      <a href="#">
+      <a href="https://www.linkedin.com/in/naved-ahmed-khan/" target="_blank">
         <FaLinkedin />
       </a>
-      <a href="#">
+      <a href="https://twitter.com/naved_js" target="_blank">
         <FaSquareXTwitter />
       </a>
-      <a href="#">
+      <a href="https://github.com/GeekyNaved" target="_blank">
         <FaGithub />
       </a>
-      <a href="#">
+      <a href="https://www.instagram.com/_naved.ahmed/?igsh=NGdsbjk3NXRyaWt1" target="_blank">
         <FaInstagramSquare />
       </a>
     </div>
