dde98f64a0946f7e1e14d8ccb777219284226650
GeekyNaved <navedahmed040@gmail.com>
2023-12-22 14:45:02 +0530

    adding icon in Download resume button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,15 @@
 import Naved_Resume from "../Naved_Resume.pdf"
-
+import { FiDownload } from "react-icons/fi"
 const Navbar = () => {
   return (
     <div className="p-3 flex justify-between items-center">
       <h4 className="text-3xl text-slate-100 font-serif">NK</h4>
-      <a href={Naved_Resume} className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md" download>Resume</a>
+      <a href={Naved_Resume} className="flex items-center gap-2 bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md" download>
+        <FiDownload />
+        <button>
+          Resume
+        </button>
+      </a>
     </div>
   );
 };
