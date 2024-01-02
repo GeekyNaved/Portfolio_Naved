43ab3940699ecb232ee99bc45e36ebfa20753603
GeekyNaved <navedahmed040@gmail.com>
2024-01-03 00:48:05 +0530

    updating horizontal padding

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import AboutSection from "./components/AboutSection";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
+import ProjectsSection from "./components/ProjectsSection";
 
 function App() {
   return (
-    <main className="px-10 bg-gray-900 text-white min-h-screen">
+    <main className="px-5 md:px-8 bg-gray-900 text-white min-h-screen">
       <Navbar />
       <HeroSection />
       <AboutSection />
+      <ProjectsSection />
     </main>
   );
 }
