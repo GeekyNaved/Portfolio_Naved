f640b903ec71c4ab79b4d6ff87cd7c3f7d864f4b
GeekyNaved <navedahmed040@gmail.com>
2024-01-03 00:46:27 +0530

    adding projects section (IN_PROGRESS)

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const ProjectCard = ({ title, description, link, image, techStack }) => {
+    return (
+        <div className='bg-gradient-to-r from-cyan-500 text- to-teal-500 px-5 py-3 mt-8 rounded'>
+            {image}
+            <div className="pt-2 flex justify-between items-center">
+                <p className='text-gray-950 text-3xl font-extrabold pt-2'>
+                    {title}
+                </p>
+                <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">
+                    {techStack}
+                </span>
+            </div>
+            <p className='py-2 text-gray-600'>
+                {description}
+            </p>
+            <div className="pt-2">
+                <a href={link} target='_blank' className='underline font-semibold'>
+                    {link ? "Live Link" : "In Progress"}
+                </a>
+
+
+            </div>
+        </div>
+
+    )
+}
+
+export default ProjectCard
