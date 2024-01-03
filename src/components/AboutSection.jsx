f40c30bfd1218e9b3a446ffa49b470591b2d8e9c
GeekyNaved <navedahmed040@gmail.com>
2024-01-03 12:51:01 +0530

    About Section -> different width UI issue resolved by adding flex-1

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,11 +1,11 @@
 import { MdEmail } from "react-icons/md"
 const AboutSection = () => {
     return (
-        <div className="relative mx-auto  py-10">
+        <div className="relative mx-auto py-10">
             <h2 className="text-3xl">About Me</h2>
-            <div className="flex flex-col gap-3 md:flex-row">
-                <p>I am extremely proficient in Front-End Development using HTML5, Cascade Style Sheets (CSS3), Bootstrap, Advance Javascript, React.Js & React Native And apart from Front-End, I have good knowledge in Back-End using Node JS, Express JS & MongoDB</p>
-                <div>
+            <div className="flex flex-col gap-5 md:flex-row md:gap-10">
+                <p className="flex-1">I am extremely proficient in Front-End Development using HTML5, Cascade Style Sheets (CSS3), Bootstrap, Advance Javascript, React.Js & React Native And apart from Front-End, I have good knowledge in Back-End using Node JS, Express JS & MongoDB</p>
+                <div className="flex-1">
                     <h4 className="text-2xl">Any Type Of Query & Discussion</h4>
                     <a
                         href="mailto:navedahmed040@gmail.com"
