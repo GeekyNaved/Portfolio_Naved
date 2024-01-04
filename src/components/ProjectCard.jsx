411a0e53875f84230e7a4e05290633a36ee457e4
GeekyNaved <navedahmed040@gmail.com>
2024-01-04 10:50:09 +0530

    Projecst --> show only 3 projects in each row & updating UI

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,21 +2,21 @@ import React from 'react'
 
 const ProjectCard = ({ title, description, link, image, techStack }) => {
     return (
-        <div className='bg-gradient-to-r from-cyan-500 text- to-teal-500 px-5 py-3 mt-8 rounded flex-1'>
+        <div className='bg-gray-50 shadow-lg px-5 py-5 mt-8 rounded flex-1'>
             {image}
             <div className="pt-2 flex justify-between items-center">
-                <p className='text-gray-950 text-3xl font-extrabold pt-2'>
+                <p className='text-gray-900 text-3xl font-bold pt-2'>
                     {title}
                 </p>
-                <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">
+                <span className="inline-flex items-center rounded-md bg-gray-800  px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-gray-500/10">
                     {techStack}
                 </span>
             </div>
-            <p className='py-2 text-gray-600'>
+            <p className='py-2 text-gray-700 leading-8'>
                 {description}
             </p>
-            <div className="pt-2">
-                <a href={link} target='_blank' className='underline font-semibold'>
+            <div className="pt-5">
+                <a href={link} target='_blank' className='text-gray-900 underline font-semibold'>
                     {link ? "Live Link" : "In Progress"}
                 </a>
 
--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -5,31 +5,49 @@ import iFileVATImage from '../assets/ifilevat.jpg'
 import macTassosImage from '../assets/mactassos.jpg'
 
 const ProjectsSection = () => {
+    const projects = [
+        {
+            id: 1,
+            image: neutrelleImage,
+            title: "Neutrelle",
+            description: "Glasgow-based start-up neutrelle empowers car owners to reduce their carbon footprint through innovative technology. They calculate CO2 emissions based on mileage and recommend tree offsets, revolutionising eco-friendly driving for a greener future.",
+            link: "https://neutrelle.vercel.app/impact_calculator",
+            techStack: "Web App",
+        },
+        {
+            id: 2,
+            image: iFileVATImage,
+            title: "iFileVAT",
+            description: "VAT Return Submission platform for the UK businesses, simplifies the process of submitting VAT returns to HMRC and accessing related information on liabilities and payments.",
+            link: "https://ifilevat-web-app-test.vercel.app",
+            techStack: "Web App",
+        },
+        {
+            id: 3,
+            image: macTassosImage,
+            title: "MacTassos",
+            description: "Developing a branded mobile app solved challenges by offering unique features, enhancing brand visibility, and eliminating high subscription costs from third-party platforms. It ensured a cost-effective solution, maintaining brand control and enhancing the customer experience.",
+            techStack: "Mobile App",
+        },
+    ]
     return (
         <div className="relative mx-auto py-10">
             <h2 className="text-3xl">My Recent Work</h2>
-            <div className="flex flex-col gap-10 md:flex-row">
-
-                <ProjectCard
-                    image={<img src={neutrelleImage} />}
-                    title="Neutrelle"
-                    description="Glasgow-based start-up neutrelle empowers car owners to reduce their carbon footprint through innovative technology. They calculate CO2 emissions based on mileage and recommend tree offsets, revolutionising eco-friendly driving for a greener future."
-                    link="https://neutrelle.vercel.app/impact_calculator"
-                    techStack="Web App"
-                />
-                <ProjectCard
-                    image={<img src={iFileVATImage} />}
-                    title="iFileVAT"
-                    description="VAT Return Submission platform for the UK businesses, simplifies the process of submitting VAT returns to HMRC and accessing related information on liabilities and payments."
-                    link="https://ifilevat-web-app-test.vercel.app/"
-                    techStack="Web App"
-                />
-                <ProjectCard
-                    image={<img src={macTassosImage} />}
-                    title="MacTassos"
-                    description="Developing a branded mobile app solved challenges by offering unique features, enhancing brand visibility, and eliminating high subscription costs from third-party platforms. It ensured a cost-effective solution, maintaining brand control and enhancing the customer experience."
-                    techStack="Mobile App"
-                />
+            <div className="grid sm:grid-cols-3 gap-5">
+                {
+                    projects?.map((item, key) => {
+                        return (
+                            <ProjectCard
+                                key={key}
+                                image={<img src={item.image} />}
+                                title={item.title}
+                                description={item.description}
+                                link={item.link}
+                                techStack={item.techStack}
+                            />
+                        )
+                    })
+                }
             </div>
 
         </div>
