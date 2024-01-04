66f6ecde53446219594baf1d672ee294cee654ee
GeekyNaved <navedahmed040@gmail.com>
2024-01-04 15:39:18 +0530

    adding custom font family in project

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import ProjectsSection from "./components/ProjectsSection";
 
 function App() {
   return (
-    <main className="px-5 md:px-8 bg-gray-900 text-white min-h-screen">
+    <main className="font-quicksand px-5 md:px-8 bg-gray-900 text-white min-h-screen">
       <Navbar />
       <HeroSection />
       <AboutSection />
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        quicksand: "Quicksand"
+      }
+    },
   },
   plugins: [],
 }
